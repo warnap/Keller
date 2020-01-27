6e41bfdde8d19c5e4af9e8053a93eda7e675c760
spssiwy <spssiwy@gmail.com>
2020-01-27 11:04:11 +0100

    Implement function for init Device. First read temperature and pressure.

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from src.Keller import Keller
 device1 = Keller(1, port='COM1', baundrate=115200)
 
 print(device1.ReadSerialNumber())
+# for i in range(11):
+#     print(device1.ReadPressure())
+#     print(device1.ReadTemperature())
+#     # print('Pressure {} Temperature {}'.format(device1.ReadPressure(), device1.ReadTemperature()))
+#
+# print(device1.EchoTest())
 
-print(device1.EchoTest())
 
-device1.__del__()
--- src/Keller.py
+++ src/Keller.py
@@ -1,4 +1,6 @@
 import serial
+from serial.serialutil import SerialException
+import struct
 
 
 class Keller:
@@ -9,23 +11,36 @@ class Keller:
     _address = 0
     _echoOn = False
     _serial = None
+    _deviceClass = None
+    _deviceGroup = None
+    _deviceYear = None
+    _deviceWeek = None
+    _deviceStatus = None
+    _deviceFirmwareVersion = None
 
-    def __init__(self, address=250, port='', baundrate=9600):
+    def __init__(self, address=250, port=None, baundrate=9600):
         self._address = address
         self._port = port
         self._serial = serial.Serial(port=self._port)
         self._serial.baudrate = baundrate
-        self._serial.timeout = 0.05
+        self._serial.timeout = 0.02
+        self._InitDevice()
+
+    def __str__(self):
+        return 'Pressure sensor Addr:{} from {}'.format(self._address, self._port)
 
     def __del__(self):
-        self._serial.close()
+        try:
+            self._serial.close()
+        except AttributeError as e:
+            print(e)
 
-    def _CRC16(self, data: bytes):
+    def _CRC16(self, data):
         """"
         CRC-16-ModBus Algorithm
         :return: integer crc
         """
-        data = bytearray(data)
+        data = bytearray(bytes(data))
         poly = 0xA001
         crc = 0xFFFF
         for b in data:
@@ -37,46 +52,83 @@ class Keller:
                     crc = ((crc >> 1) & 0xFFFF)
         return crc
 
-    def _ArrayCrcKeller(self, data: bytes):
-        return [i for i in self._CRC16(data).to_bytes(2, byteorder='big')]
+    def _ArrayCrcKeller(self, data):
+        return [i for i in self._CRC16(bytes(data)).to_bytes(2, byteorder='big')]
 
-    def _ArrayCrcModbus(self, data: bytes):
-        return [i for i in reversed(self._ArrayCrcKeller(data))]
+    def _ArrayCrcModbus(self, data):
+        return [i for i in reversed(self._ArrayCrcKeller(bytes(data)))]
 
-    def _SendRecive(self, frame: bytes):
-        self._serial.write(frame)
-        return self._serial.read(10)
+    def _SendReceive(self, frame):
+        # todo: Create function to check receive
+        # todo: Add
+
+        self._serial.write(bytes(frame))
+        receive = self._frameToArray(self._serial.read(10))
+        if receive is not None:
+            if len(receive) == 5:
+                if receive[1] > 0x80:
+                    print('Error code = {}'.format(receive[2]))
+        return receive
 
     def _CheckCRC(self, data):
-        frame = self._frame_to_array(data)
+        frame = self._frameToArray(data)
+        crc = self._frameToArray(data[-2:])
         if frame[1] > 16:
-            crc_calc = self._ArrayCrcKeller(bytes(data[:-2]))
+            crc_calc = self._ArrayCrcKeller(data[:-2])
         else:
-            crc_calc = self._ArrayCrcModbus(bytes(data[:-2]))
-
-        if frame[1] > 0x80:  # todo: add error frame chcecked
-            print('error frame')
-        crc = self._frame_to_array(data[-2:])
+            crc_calc = self._ArrayCrcModbus(data[:-2])
 
         return True if crc == crc_calc else False
 
-    def _frame_to_array(self, frame: bytes):
+    def _frameToArray(self, frame):
         return [i for i in frame]
+
+    def _InitDevice(self):
+        frame = [self._address, 48]
+        frame += self._ArrayCrcKeller(frame)
+        receive = self._frameToArray(self._SendReceive(frame))
+
+
+        print(receive)
 
     def ReadSerialNumber(self):
         frame = [self._address, 69]
-        frame += self._ArrayCrcKeller(bytes(frame))
-        recive = self._SendRecive(bytes(frame))
-        if self._CheckCRC(recive):
-            recive = self._frame_to_array(recive[2:6])
-            recive.reverse()
+        frame += self._ArrayCrcKeller(frame)
+        receive = self._SendReceive(frame)
+        if self._CheckCRC(receive):
+            receive = self._frameToArray(receive[2:6])
+            receive.reverse()
             # SN = 256 3 * SN3 + 256 2 * SN2 + 256 * SN1 + SN0
             data = 0
             for i in range(0, 4):
-                data += 256 ** i * recive[i]
+                data += 256 ** i * receive[i]
             return data
 
     def EchoTest(self):
         frame = [self._address, 8]
+        frame += self._ArrayCrcModbus(frame)
+        pass
+
+    def ReadPressure(self):
+        frame = [self._address, 3, 0, 2, 0, 2]
+        frame += self._ArrayCrcModbus(frame)
+        receive = self._SendReceive(frame)
+        if self._CheckCRC(receive):
+            receive = self._frameToArray(receive[3:7])
+            receive.reverse()
+            return struct.unpack('f', bytes(receive))
+
+    def ReadTemperature(self):
+        frame = [self._address, 3, 0, 8, 0, 2]
         frame += self._ArrayCrcModbus(bytes(frame))
+        receive = self._SendReceive(bytes(frame))
+        if self._CheckCRC(receive):
+            receive = self._frameToArray(receive[3:7])
+            receive.reverse()
+            return struct.unpack('f', bytes(receive))
+
+    def ReadRegister(self):
+        pass
+
+    def WriteRegister(self):
         pass
