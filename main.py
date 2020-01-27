0bf33f56178715b23c9b758cedad76211b320ae5
spssiwy <spssiwy@gmail.com>
2020-01-27 13:31:58 +0100

    Added support for devices with echoOn turned on.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from src.Keller import Keller
-
+import datetime
 device1 = Keller(1, port='COM1', baundrate=115200)
 
 print(device1.ReadSerialNumber())
-# for i in range(11):
-#     print(device1.ReadPressure())
-#     print(device1.ReadTemperature())
-#     # print('Pressure {} Temperature {}'.format(device1.ReadPressure(), device1.ReadTemperature()))
-#
-# print(device1.EchoTest())
+for i in range(101):
+    print(datetime.datetime.now(), device1.ReadPressure())
+    # print(device1.ReadTemperature())
+    # print('Pressure {} Temperature {}'.format(device1.ReadPressure(), device1.ReadTemperature()))
+
+print(device1._echoOn)
 
 
--- src/Keller.py
+++ src/Keller.py
@@ -1,5 +1,4 @@
 import serial
-from serial.serialutil import SerialException
 import struct
 
 
@@ -24,16 +23,14 @@ class Keller:
         self._serial = serial.Serial(port=self._port)
         self._serial.baudrate = baundrate
         self._serial.timeout = 0.02
+        self._EchoTest()
         self._InitDevice()
 
     def __str__(self):
         return 'Pressure sensor Addr:{} from {}'.format(self._address, self._port)
 
     def __del__(self):
-        try:
-            self._serial.close()
-        except AttributeError as e:
-            print(e)
+        self._serial.close()
 
     def _CRC16(self, data):
         """"
@@ -58,16 +55,20 @@ class Keller:
     def _ArrayCrcModbus(self, data):
         return [i for i in reversed(self._ArrayCrcKeller(bytes(data)))]
 
-    def _SendReceive(self, frame):
+    def _SendReceive(self, frame, receiveLenght=10):
         # todo: Create function to check receive
-        # todo: Add
-
         self._serial.write(bytes(frame))
-        receive = self._frameToArray(self._serial.read(10))
+        if self._echoOn:
+            receive = self._frameToArray(self._serial.read(receiveLenght + len(frame)))
+            receive = receive[len(frame):]
+        else:
+            receive = self._frameToArray(self._serial.read(receiveLenght))
+
         if receive is not None:
             if len(receive) == 5:
                 if receive[1] > 0x80:
                     print('Error code = {}'.format(receive[2]))
+
         return receive
 
     def _CheckCRC(self, data):
@@ -88,7 +89,6 @@ class Keller:
         frame += self._ArrayCrcKeller(frame)
         receive = self._frameToArray(self._SendReceive(frame))
 
-
         print(receive)
 
     def ReadSerialNumber(self):
@@ -104,10 +104,17 @@ class Keller:
                 data += 256 ** i * receive[i]
             return data
 
-    def EchoTest(self):
-        frame = [self._address, 8]
+    def _EchoTest(self):
+        frame = [self._address, 8, 0, 0, 1, 1]
         frame += self._ArrayCrcModbus(frame)
-        pass
+        receive = self._frameToArray(self._SendReceive(frame, receiveLenght=len(frame) * 2))
+        if len(receive) > len(frame):
+            receive1 = receive[:len(frame)]
+            receive2 = receive[len(frame):]
+            if frame == receive1 == receive2:
+                self._echoOn = True
+        else:
+            self._echoOn = False
 
     def ReadPressure(self):
         frame = [self._address, 3, 0, 2, 0, 2]
@@ -116,19 +123,22 @@ class Keller:
         if self._CheckCRC(receive):
             receive = self._frameToArray(receive[3:7])
             receive.reverse()
-            return struct.unpack('f', bytes(receive))
+            data = struct.unpack('f', bytes(receive))
+            return data[0]
 
     def ReadTemperature(self):
         frame = [self._address, 3, 0, 8, 0, 2]
-        frame += self._ArrayCrcModbus(bytes(frame))
-        receive = self._SendReceive(bytes(frame))
+        frame += self._ArrayCrcModbus(frame)
+        receive = self._SendReceive(frame)
         if self._CheckCRC(receive):
             receive = self._frameToArray(receive[3:7])
             receive.reverse()
-            return struct.unpack('f', bytes(receive))
+            data = struct.unpack('f', bytes(receive))
+            return data[0]
 
     def ReadRegister(self):
         pass
 
     def WriteRegister(self):
         pass
+
